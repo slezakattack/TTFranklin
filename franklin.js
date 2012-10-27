c288545f64f92c2f0c7cd60175ad2fa1a13629df
Michael Slezak <michael_slezak08@hotmail.com>
2012-10-27 11:44:59 -0700

    adding basic functionality to franklin

--- franklin.js
+++ franklin.js
@@ -1,1 +1,40 @@
-var Bot = module.exports = require('./bot').Bot;
+global.fs = require('fs');
+global.url = require('url');
+
+global.Bot;
+global.bot;
+global.config;
+
+initializeFranklin();
+
+console.log("Welcome to Franklin the big sleaze bot!");
+
+function initializeFranklin() {
+	try {
+		Bot = require('ttapi');
+	} catch(e) {
+		console.log(e);
+		console.log('Check to see that you have the ttapi package installed for node.js.');
+	}
+	
+	var args = process.argv;
+	config = loadConfigFile();	
+	bot = new Bot(config.bot_properties.auth, config.bot_properties.user_id, config.bot_properties.room_id);
+/*	bot.on('speak', function(data) {
+		var name = data.name;
+		var text = data.text;
+		
+	});*/
+	bot.speak("Hey michael and big phil!");
+}
+
+function loadConfigFile() {
+	try {
+		var config = JSON.parse(fs.readFileSync('config.json', 'ascii'));
+	} catch(e) {
+		console.log('ERROR: Couldn\'t load the config.json file.');
+		console.log(e);
+		return {};
+	}
+	return config;
+}
